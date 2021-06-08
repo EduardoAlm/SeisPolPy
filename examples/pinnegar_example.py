bffc3e82c19cf282eb017564cda8936dbf1c95ef
MrEdwards <eduardorodalm95@gmail.com>
2021-06-08 16:33:27 +0100

    update on tests

--- examples/pinnegar_example.py
+++
@@ -1,8 +0,0 @@
-import SeisPolPy
-import mat4py
-import numpy as np
-
-
-data = mat4py.loadmat('examples/ACRG.mat') # seismic data
-sig = np.array([data['t'], data['r'], data['z']])
-SeisPolPy.Pinnegar.pinnegar(sig)
