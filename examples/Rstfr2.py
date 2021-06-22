98040aa24f701ed275e3ee59e9fd2ce7bb84fac9
MrEdwards <eduardorodalm95@gmail.com>
2021-06-22 20:21:41 +0100

    update

--- examples/Rstfr2.py
+++ examples/Rstfr2.py
@@ -7,18 +7,21 @@ RSTFR Method.
     The MIT License (MIT)
     Copyright (c) 2021 MrEdwards
 """
-import numpy as np
+from numpy import abs, int16, maximum, conj, ceil, flipud, add, concatenate, reshape, int32, zeros, array, real, sqrt, dot, multiply, linspace, exp, float64, ones, pi, transpose, float16
+from numpy.linalg import eigh
+from numpy import abs as abso
+from numpy import max as maxim
 import matplotlib.pyplot as plt
 import time
 import base64
 import io
 start_time = time.time()
-import scipy.sparse as sp
-import forw_op
-import adjoint
-import math
-DTYPE = np.float64
-import diags
+from scipy.sparse import dia_matrix
+import forw_op  
+import adjoint 
+from math import cos
+
+import diags  
 
 
 def soft_threshholding(z, T):
@@ -31,9 +34,9 @@ def soft_threshholding(z, T):
     :type T: array
     :return: array with absolute values
     """
-    az = np.abs(z)
-    res = np.maximum(az-T, 0)/(np.maximum(az-T,0)+T)*z
-    return np.abs(res)
+    az = abso(z)
+    res = maximum(az-T, 0)/(maximum(az-T,0)+T)*z
+    return abso(res)
 
 def cross(x1, x2):
     """
@@ -45,13 +48,13 @@ def cross(x1, x2):
     :type x2: array
     :return: numpy array
     """
-    x = x1 * np.conj(x2)
+    x = x1 * conj(x2)
     length = len(x1)
-    half = np.ceil(length/2).astype(np.int32)
+    half = ceil(length/2).astype(int32)
     fstpart = x[1:(half)]
-    sndpart = np.flipud(x[(half+1):])
-    sumofparts = np.add(fstpart, sndpart)
-    corr = np.concatenate(([x[0]], sumofparts))/length
+    sndpart = flipud(x[(half+1):])
+    sumofparts = add(fstpart, sndpart)
+    corr = concatenate(([x[0]], sumofparts))/length
     return corr
 
 def semimm(t, r, z):
@@ -68,61 +71,51 @@ def semimm(t, r, z):
     """
     
     length = len(t)
-    half = math.ceil(length/2)
-    semi = np.zeros((12, half))
-    major = np.zeros((3, half))
-    minor = np.zeros((3, half))
-    tt = cross(t, t).astype(np.float64)
-    rr = cross(r, r).astype(np.float64)
-    zz = cross(z, z).astype(np.float64)
-    tr = cross(t, r).astype(np.float64)
-    tz = cross(t, z).astype(np.float64)
-    rz = cross(r, z).astype(np.float64)
-    
-    for i in range(half):
-        cov_matrix = np.array([[tt[i], tr[i], tz[i]],[tr[i], rr[i], rz[i]],[tz[i],rz[i], zz[i]]], dtype='float64')
-        eig_values, V = np.linalg.eigh(cov_matrix)
+    half = ceil(length/2).astype(int16)
+    print(half)
+    semi,major,minor = zeros((12, half)),zeros((3, half)),zeros((3, half))
+    tt, rr,zz,tr,tz,rz, i= cross(t, t),cross(r, r),cross(z, z),cross(t, r),cross(t, z),cross(r, z),0
+ 
+    while i < half:
+        eig_values, V = eigh(array([[tt[i], tr[i], tz[i]],[tr[i], rr[i], rz[i]],[tz[i],rz[i], zz[i]]], dtype='float64'))
         #V = np.transpose(V)
-        eig_values = np.real(eig_values)
+        eig_values = real(eig_values)
         semi[0:3, i] = V[:, 2]
         semi[3:6, i] = V[:, 1]
         semi[6:9, i] = V[:, 0]
         semi[9:, i] = eig_values
-        facc = np.sqrt(length)/np.sqrt(2)
-        major[:, i] = np.dot(np.dot(facc, np.sqrt(np.abs(eig_values[2]))), V[:, 2])
-        minor[:, i] = np.dot(np.dot(facc, np.sqrt(np.abs(eig_values[1]))), V[:, 1])
+        facc = sqrt(length)/sqrt(2)
+        major[:, i], minor[:, i] = dot(dot(facc, sqrt(abso(eig_values[2]))), V[:, 2]), dot(dot(facc, sqrt(abso(eig_values[1]))), V[:, 1])
+         
+        i+=1
         
-    major_norm = major/np.max(np.abs(major))
-    minor_norm = minor/np.max(np.abs(minor))
-    return semi, major, minor, major_norm, minor_norm
+    return semi, major, minor, major/maxim(abso(major)), minor/maxim(abso(minor))
 
 def forward(N, s):
-    """
-    Forward function calls the Cshared library for the diags function to create a diagonal \
-    sparse matrix.
-    
+    """import
     :parameter N: length of component array "x"
     :type N: int
     :parameter s: with default value of S equal to 100.
     :type s: int
     :return: scipy sparse dia matrix. 
     """
-    id = np.linspace(0, N-1, N, dtype="int32")
-    w = np.zeros([N, N], dtype=DTYPE)
+    ids, w = linspace(0, N-1, N, dtype=int32), zeros([N, N], dtype=float64)
+    
     if type(s) == int:
-        sj = s * np.ones([N, 1]).astype(int)
+        sj = s * ones([N, 1]).astype(int)
     elif len(s) != N:
         raise Exception('length of "s" should be one or the same as length of data ')
+    i=0
+    while i < N:
+        w[:, i] = exp(multiply(-2 * pow(pi, 2), pow(ids - i, 2)) / pow(sj[i], 2))
+        i+=1
 
-    for i in range(N-1):
-        w[:, i] = np.exp(np.multiply(-2 * pow(np.pi, 2), pow(id - i, 2)) / pow(sj[i], 2))
+    offset = linspace(0, pow(N, 2) - N, N, dtype=int32)
 
-    offset = np.linspace(0, pow(N, 2) - N, N, dtype="int32")
+    y = transpose(w)
 
-    y = np.transpose(w)
-
-    res = diags.diagonal(y.tolist(), offset,  N, pow(N, 2), dtype=DTYPE)
-    return sp.dia_matrix((res, offset), shape=(N, pow(N, 2)))
+    res = diags.diagonal(y.tolist(), offset,  N, pow(N, 2), dtype=float64)
+    return dia_matrix((res, offset), shape=(N, pow(N, 2)))
 
 def stft_s_ist(x, y, z, s, n_it, mu):
     """
@@ -148,25 +141,26 @@ def stft_s_ist(x, y, z, s, n_it, mu):
     N = len(x)
     d1, d2, d3 = x, y, z
     dh1, dh2, dh3 = d1, d2, d3
-    tfx, tfy, tfz = np.zeros([pow(N, 2), 1], dtype=float), np.zeros([pow(N, 2), 1], dtype=float), np.zeros([pow(N, 2), 1], dtype=float)
+    tfx, tfy, tfz = zeros([pow(N, 2), 1], dtype=float), zeros([pow(N, 2), 1], dtype=float), zeros([pow(N, 2), 1], dtype=float)
    
     
     G = forward(N, s)
     
-    lamb_da = np.multiply(np.multiply(mu, np.abs(adjoint.adjoin(G, N, d1)).max(0)), 1.2) 
-    
-    for i in range(n_it):
-        U1j = tfx + np.multiply(mu, adjoint.adjoin(G, N, (d1 - forw_op.forw_o(G, N, tfx))))
-        U2j = tfy + np.multiply(mu, adjoint.adjoin(G, N, (d2 - forw_op.forw_o(G, N, tfy))))
-        U3j = tfz + np.multiply(mu, adjoint.adjoin(G, N, (d3 - forw_op.forw_o(G, N, tfz))))
+    lamb_da = multiply(multiply(mu, abso(adjoint.adjoin(G, N, d1)).max(0)), 1.2) 
+    i=0
+    while i < n_it:
+        U1j = tfx + multiply(mu, adjoint.adjoin(G, N, (d1 - forw_op.forw_o(G, N, tfx))))
+        U2j = tfy + multiply(mu, adjoint.adjoin(G, N, (d2 - forw_op.forw_o(G, N, tfy))))
+        U3j = tfz + multiply(mu, adjoint.adjoin(G, N, (d3 - forw_op.forw_o(G, N, tfz))))
         tfx = soft_threshholding(U1j, lamb_da)
         tfy = soft_threshholding(U2j, lamb_da)
         tfz = soft_threshholding(U3j, lamb_da)
-        d1 = d1 + (dh1 - np.real(forw_op.forw_o(G, N, tfx)))
-        d2 = d2 + (dh2 - np.real(forw_op.forw_o(G, N, tfy)))
-        d3 = d3 + (dh3 - np.real(forw_op.forw_o(G, N, tfz)))
+        d1 = d1 + (dh1 - real(forw_op.forw_o(G, N, tfx)))
+        d2 = d2 + (dh2 - real(forw_op.forw_o(G, N, tfy)))
+        d3 = d3 + (dh3 - real(forw_op.forw_o(G, N, tfz)))
+        i+=1
     
-    return np.reshape(tfx, (N, N)), np.reshape(tfy, (N, N)), np.reshape(tfz, (N, N))
+    return reshape(tfx, (N, N)), reshape(tfy, (N, N)), reshape(tfz, (N, N))
 
 def stft(x, s):
     """
@@ -178,11 +172,11 @@ def stft(x, s):
     :type s: int
     :return: component data array after applying the STFT.  
     """
-    N = len(x)
+    N = len(x), 
     G = forward(N, s)
     tfx = adjoint.adjoin(G, N, x)
 
-    return np.reshape(tfx,(N, N))
+    return reshape(tfx,(N, N))
 
 def amplitude(zeta, eta, eig_values):
     """
@@ -197,12 +191,12 @@ def amplitude(zeta, eta, eig_values):
     :return: array containing the obtained directivity values. 
     """
     
-    amp = eig_values[2]
-    if amp >= 0 and amp < zeta:
+   
+    if eig_values[2] >= 0 and eig_values[2] < zeta:
         ampli = 0.
-    elif amp > zeta and amp < eta:
-        ampli = math.cos(math.pi*(amp-zeta)/2*(eta-zeta))
-    elif amp > eta and amp <= 1:
+    elif eig_values[2] > zeta and eig_values[2] < eta:
+        ampli = cos(pi*(eig_values[2]-zeta)/2*(eta-zeta))
+    elif eig_values[2] > eta and eig_values[2] <= 1:
         ampli = 1.
     else:
         raise Exception("Amplitude out of bounds when aplying the rectilinearity filter.")
@@ -221,14 +215,14 @@ def directivity_love(gamma, lamb_da, eig_vec3):
     :return: the obtained directivity value for the love wave. 
     """
     print("dir_love")
-    b_vec=np.array([[1,0,0],[0,1,0],[0,0,1]], np.int)
+    b_vec=array([[1,0,0],[0,1,0],[0,0,1]], int)
 
-    degree_res = np.max(np.abs(np.dot(eig_vec3[2], b_vec[0]))) 
+    degree_res = maxim(abso(dot(eig_vec3[2], b_vec[0]))) 
     degree_dir = float("{:.2f}".format(round(degree_res, 2)))      
     if degree_dir >= 0 and degree_dir < gamma:
         degree_res_dir = 1.
     elif degree_dir > gamma and degree_dir < lamb_da:
-        degree_res_dir = math.cos(math.pi*(degree_dir-gamma)/2*(lamb_da-gamma))
+        degree_res_dir = cos(pi*(degree_dir-gamma)/2*(lamb_da-gamma))
     elif degree_dir > lamb_da and degree_dir <= 1:
         degree_res_dir = 0.
     else:
@@ -248,17 +242,16 @@ def directivity_rayleigh(gamma, lamb_da, eig_vec3):
     :type eig_vec3: array
     :return: array containing the obtained directivity values for the rayleigh wave. 
     """
-    print("dir_ray")
-    b_vec = np.array([[1,0,0],[0,1,0],[0,0,1]], np.int)
+    b_vec = array([[1,0,0],[0,1,0],[0,0,1]], int)
 
-    radial = np.max(np.abs(np.dot(eig_vec3, b_vec[1])))
-    z = np.max(np.abs(np.dot(eig_vec3, b_vec[2])))
-    degree_res = math.sqrt(radial+z)         
+    radial, z = maxim(abso(dot(eig_vec3, b_vec[1]))), maxim(abso(dot(eig_vec3, b_vec[2])))
+    
+    degree_res = sqrt(radial+z)         
     degree_dir = float("{:.2f}".format(round(degree_res, 2)))
     if degree_dir >= 0 and degree_dir < gamma:
         degree_res_dir = 1.
     elif degree_dir > gamma and degree_dir < lamb_da:
-        degree_res_dir = math.cos(math.pi*(degree_dir-gamma)/2*(lamb_da-gamma))
+        degree_res_dir = cos(pi*(degree_dir-gamma)/2*(lamb_da-gamma))
     elif degree_dir > lamb_da and degree_dir <= 1:
         degree_res_dir = 0.
     else:
@@ -283,7 +276,7 @@ def rectilinearity(alpha, beta, eig_values):
     if degree_rec >= -1 and degree_rec < alpha:
         rec = 1.
     elif degree_rec > alpha and degree_rec < beta:
-        rec = math.cos(math.pi*(degree_rec-alpha)/2*(beta-alpha))
+        rec = cos(pi*(degree_rec-alpha)/2*(beta-alpha))
     elif degree_rec > beta and degree_rec <= 1:
         rec = 0.
     else:
@@ -329,9 +322,8 @@ def rstfr(data, alg="stft",filt="love", s=100, n_it=400, alpha=0.1, beta=0.12, g
         bytes containing the previous arrays plots.
     """
 
-    sig = data
-    start_time = time.time()
- 
+    sig, start_time = data, time.time()
+
     plt.rcParams['figure.figsize'] = [13, 8]
     plt.rcParams.update({'font.size': 18})
 
@@ -341,39 +333,42 @@ def rstfr(data, alg="stft",filt="love", s=100, n_it=400, alpha=0.1, beta=0.12, g
 
     if alg == "stft":
         # Obtaining the Short Time Fourier Transform for each component
-        tfrx = stft(r, s)  # R component vector 
-        tfry = stft(t, s)  # T component vector 
-        tfrz = stft(z, s)  # Z component vector 
+        tfrx, tfry, tfrz = stft(r, s),  stft(t, s), stft(z, s) # R component vector 
+  
     elif alg == "s_stft":
         # Obtaining the sparse Short Time Fourier Transform for each component
         tfrx, tfry, tfrz = stft_s_ist(r, t, z, s, n_it, mu)  
     else:
         raise Exception("Please choose an available method (stft/s_stft).")
 
-    nf = np.ceil(N/2).astype(int)
-    semi, majo, mino, majon, minon = np.zeros((12,nf,N)), np.zeros((3,nf,N)), np.zeros((3,nf,N)), \
-        np.zeros((3,nf,N)), np.zeros((3,nf,N))
-
-    for i in range(N):
+    nf = ceil(N/2).astype(int)
+    semi, majo, mino, majon, minon,i  = zeros((12,nf,N)), zeros((3,nf,N)), zeros((3,nf,N)), \
+        zeros((3,nf,N)), zeros((3,nf,N)), 0
+    
+    while i < N:
         # Generates the semi major/minor
-        semi[:,:,i], majo[:,:,i], mino[:,:,i], majon[:,:,i], minon[:,:,i] = semimm(np.multiply(10, tfry[:,i]),\
-            np.multiply(10, tfrx[:,i]),np.multiply(10, tfrz[:,i]))
+        semi[:,:,i], majo[:,:,i], mino[:,:,i], majon[:,:,i], minon[:,:,i] = semimm(multiply(10, tfry[:,i]),\
+            multiply(10, tfrx[:,i]), multiply(10, tfrz[:,i]))
+        i+=1
         #print("Generating the semi major/minor ... ",np.round(i*100/N, 2), "%")
-    print(semi[0][0,0])
-    n2 = len(semi[0, 0]) #last part
-    n = len(semi[0]) #middle part
-    
-    majornorm, minornorm = np.zeros((nf, N)), np.zeros((nf, N))
-    
-    for i in range(nf):
-        for j in range(N):
-            # Normalizes the semi major and minor values
-            majornorm[i, j], minornorm[i, j] = np.sqrt(np.dot(majo[:, i, j],majo[:, i, j])), np.sqrt(np.dot(mino[:, i, j],mino[:, i, j]))
-        print("Normalising the output values ... ",np.round(i*100/nf, 2), "%")
 
-    cax = np.max((np.max(majornorm), np.max(minornorm)))
+    n2, n = len(semi[0, 0]), len(semi[0])
+   
     
-    plt.imshow(np.abs(majornorm), cmap='hot', alpha=1, vmin=0, vmax=0.7*cax)
+    majornorm, minornorm, i, j= zeros((nf, N)), zeros((nf, N)), 0, 0
+     
+    while i < nf:
+        while j < N:
+            
+            # Normalizes the semi major and minor values
+            majornorm[i, j], minornorm[i, j] = sqrt(dot(majo[:, i, j],majo[:, i, j])), sqrt(dot(mino[:, i, j],mino[:, i, j]))
+            j += 1
+        i+=1
+        print("Normalising the output values ... ",round(i*100/nf, 2), "%")
+
+    cax = maxim((maxim(majornorm), maxim(minornorm)))
+    
+    plt.imshow(abso(majornorm), cmap='hot', alpha=1, vmin=0, vmax=0.7*cax)
     plt.title('RS-TFR (SM)')    
    
     StringIObytes1 = io.BytesIO()
@@ -382,7 +377,7 @@ def rstfr(data, alg="stft",filt="love", s=100, n_it=400, alpha=0.1, beta=0.12, g
     StringIObytes1.seek(0)
     b64jpgdataMajor = base64.b64encode(StringIObytes1.read()).decode()
     plt.close()
-    plt.imshow(np.abs(minornorm), cmap="hot", alpha=1, vmin=0, vmax=0.7*cax)
+    plt.imshow(abso(minornorm), cmap="hot", alpha=1, vmin=0, vmax=0.7*cax)
     plt.title('RS-TFR (Sm)')
    
     StringIObytes2 = io.BytesIO()
@@ -405,14 +400,12 @@ def rstfr(data, alg="stft",filt="love", s=100, n_it=400, alpha=0.1, beta=0.12, g
     plt.show()
     plt.close()
     """
-    rec_filter = np.zeros((n2, n2), dtype=np.float16)
-    amp_filter = np.zeros((n2, n2), dtype=np.float16)
-    dir_love_filter = np.zeros((n2,n2), dtype=np.float16)
-    dir_rayleigh_filter = np.zeros((n2, n2), dtype=np.float16)
-    for i in range(n):
-        for j in range(n2):
-            eig_values = np.array([semi[9][0,0],semi[10][0,0],semi[11][0,0]])
-            biggesteig_vec=np.array([semi[6][0,0],semi[7][0,0], semi[8][0,0]])        
+    rec_filter, amp_filter, dir_love_filter, dir_rayleigh_filter, i, j= zeros((n2, n2), dtype=float16), zeros((n2, n2), dtype=float16), zeros((n2,n2), dtype=float16), zeros((n2, n2), dtype=float16), 0, 0
+ 
+    while i < n:
+        while j < n2:
+            eig_values = array([semi[9][0,0],semi[10][0,0],semi[11][0,0]])
+            biggesteig_vec=array([semi[6][0,0],semi[7][0,0], semi[8][0,0]])        
             rec_filter[j, j] = rectilinearity(alpha, beta, eig_values)
             amp_filter[j, j] = amplitude(zeta, eta, eig_values)
 
@@ -422,34 +415,36 @@ def rstfr(data, alg="stft",filt="love", s=100, n_it=400, alpha=0.1, beta=0.12, g
                 dir_rayleigh_filter[j, j] = directivity_rayleigh(gamma, lamb_da, biggesteig_vec)
             else:
                 raise Exception("The chosen options is not available please check the documentation.")
+            j += 1
+        i+=1
     if filt=="love":
-        rej_love_filter = 1 - np.multiply((1 - rec_filter), (1 - dir_love_filter), (1 - amp_filter))
-        ext_love_filter = np.multiply((1 - rec_filter), (1 - dir_love_filter), (1 - amp_filter))
-        rejected_x = np.dot(tfrx, rej_love_filter)
-        rejected_y = np.dot(tfry, rej_love_filter)
-        rejected_z = np.dot(tfrz, rej_love_filter)
-        extracted_x = np.dot(tfrx, ext_love_filter)
-        extracted_y = np.dot(tfry, ext_love_filter)
-        extracted_z = np.dot(tfrz, ext_love_filter)
+        rej_love_filter = 1 - multiply((1 - rec_filter), (1 - dir_love_filter), (1 - amp_filter))
+        ext_love_filter = multiply((1 - rec_filter), (1 - dir_love_filter), (1 - amp_filter))
+        rejected_x = dot(tfrx, rej_love_filter)
+        rejected_y = dot(tfry, rej_love_filter)
+        rejected_z = dot(tfrz, rej_love_filter)
+        extracted_x = dot(tfrx, ext_love_filter)
+        extracted_y = dot(tfry, ext_love_filter)
+        extracted_z = dot(tfrz, ext_love_filter)
     elif filt=="rayleigh":
-        rej_rayleigh_filter = 1 - np.multiply((1 - rec_filter), (1 - dir_rayleigh_filter), (1 - amp_filter))
-        ext_rayleigh_filter = np.multiply((1 - rec_filter), (1 - dir_rayleigh_filter), (1 - amp_filter))
-        rejected_x = np.multiply(tfrx, rej_rayleigh_filter)
-        rejected_y = np.multiply(tfry, rej_rayleigh_filter)
-        rejected_z = np.multiply(tfrz, rej_rayleigh_filter)
-        extracted_x = np.multiply(tfrx, ext_rayleigh_filter)
-        extracted_y = np.multiply(tfry, ext_rayleigh_filter)
-        extracted_x = np.multiply(tfrz, ext_rayleigh_filter)
+        rej_rayleigh_filter = 1 - multiply((1 - rec_filter), (1 - dir_rayleigh_filter), (1 - amp_filter))
+        ext_rayleigh_filter = multiply((1 - rec_filter), (1 - dir_rayleigh_filter), (1 - amp_filter))
+        rejected_x = multiply(tfrx, rej_rayleigh_filter)
+        rejected_y = multiply(tfry, rej_rayleigh_filter)
+        rejected_z = multiply(tfrz, rej_rayleigh_filter)
+        extracted_x = multiply(tfrx, ext_rayleigh_filter)
+        extracted_y = multiply(tfry, ext_rayleigh_filter)
+        extracted_x = multiply(tfrz, ext_rayleigh_filter)
     else:
         raise Exception("The chosen options is not available please check the documentation.")            
     
     
     plt.figure()
-   # f, ax = plt.subplots(3,1) 
-   # ax[0].imshow(tfrx)
-   # ax[1].imshow(tfry)
-   # ax[2].imshow(tfrz)
-   # plt.title("stft_s_ist components")
+   #f, ax = plt.subplots(3,1) 
+   #ax[0].imshow(tfrx)
+   #ax[1].imshow(tfry)
+   #ax[2].imshow(tfrz)
+   #plt.title("stft_s_ist components")
     f, ax2 = plt.subplots(3,1) 
     ax2[0].imshow(rejected_x)
     ax2[1].imshow(rejected_y)
@@ -461,7 +456,7 @@ def rstfr(data, alg="stft",filt="love", s=100, n_it=400, alpha=0.1, beta=0.12, g
   
     StringIObytes3.seek(0)
     b64jpgdataextraction = base64.b64encode(StringIObytes3.read()).decode()
-    plt.show()
+    #plt.show()
     plt.close()
     f, ax3 = plt.subplots(3,1) 
     ax3[0].imshow(extracted_x)
@@ -474,7 +469,7 @@ def rstfr(data, alg="stft",filt="love", s=100, n_it=400, alpha=0.1, beta=0.12, g
   
     StringIObytes4.seek(0)
     b64jpgdatarejection = base64.b64encode(StringIObytes4.read()).decode()
-    plt.show()
+    #plt.show()
     plt.close()
 
 
--- examples/rstfr_s_stft_example.py
+++ examples/rstfr_s_stft_example.py
@@ -6,23 +6,24 @@ import base64
 import io
 import matplotlib.image as mpimg
 
-data = mat4py.loadmat('examples/dataII.ABKTtestspstft.mat') # seismic data
+data = mat4py.loadmat('dataII.ABKTtestspstft.mat') # seismic data
 sig = np.array([data['t'], data['r'], data['z']])
 b64dataM, b64datam, b64dataext, b64datarej = rstfr(sig, "s_stft", "love", 100, 400, 0.1, 0.12, 0.13, 0.26, 0.26, 0.23)
 
-f = open("examples/outputRstfrB64_S_STFTMajor.txt", "w")
+f = open("outputRstfrB64_S_STFTMajor.txt", "w")
 f.write(b64dataM)
 f.close()
-f1 = open("examples/outputRstfrB64_S_STFTMinor.txt", "w")
+f1 = open("outputRstfrB64_S_STFTMinor.txt", "w")
 f1.write(b64datam)
 f1.close()
-f2 = open("examples/outputRstfrB64_S_STFTextraction.txt", "w")
+f2 = open("outputRstfrB64_S_STFTextraction.txt", "w")
 f2.write(b64dataext)
 f2.close()
-f3 = open("examples/outputRstfrB64_S_STFTrejection.txt", "w")
+f3 = open("outputRstfrB64_S_STFTrejection.txt", "w")
 f3.write(b64datarej)
 f3.close()
 
+"""
 
 imageM = base64.b64decode(b64dataM)
 imageM = io.BytesIO(imageM)
@@ -37,14 +38,30 @@ imagem = mpimg.imread(imagem, format='jpg')
 plt.imshow(imagem, interpolation='nearest')
 plt.show()
 
+imageM = base64.b64decode(b64dataext)
+imageM = io.BytesIO(imageM)
+imageM = mpimg.imread(imageM, format='jpg')
+plt.imshow(imageM, interpolation='nearest')
+plt.show()
+plt.close()
+
 imagem = base64.b64decode(b64datarej)
 imagem = io.BytesIO(imagem)
 imagem = mpimg.imread(imagem, format='jpg')
 plt.imshow(imagem, interpolation='nearest')
 plt.show()
 
-imagem = base64.b64decode(b64dataext)
+f = open("examples/outputRstfrB64_S_STFTextraction.txt", "r")
+f2 = open("examples/outputRstfrB64_S_STFTrejection.txt", "r")
+imagem = base64.b64decode(f.read())
 imagem = io.BytesIO(imagem)
 imagem = mpimg.imread(imagem, format='jpg')
 plt.imshow(imagem, interpolation='nearest')
 plt.show()
+
+imagem = base64.b64decode(f2.read())
+imagem = io.BytesIO(imagem)
+imagem = mpimg.imread(imagem, format='jpg')
+plt.imshow(imagem, interpolation='nearest')
+plt.show()
+"""
